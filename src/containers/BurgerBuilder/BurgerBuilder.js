971e9849351e7057220035464066f25a266e40c6
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-08-30 01:47:37 +0530

    Video 128

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Auxilliary from '../../components/hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+     render() {
+         return (
+             <Auxilliary>
+                 <Burger />
+                 <div>Build Controls</div>
+             </Auxilliary>
+         )
+     }
+}
+
+export default BurgerBuilder;
