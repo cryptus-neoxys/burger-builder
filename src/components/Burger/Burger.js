93a976e80007e8a086a5d65389830c5dc4c58344
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-08-30 15:00:11 +0530

    Video 129

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngridient/BurgerIngredient'
 
 const burger = (props) => {
-  return(
+	// Extract each ingredient .. for eg : Bacon, cheese, salad
+	const transformedIngredients = Object.keys(
+		props.ingredients).map(igKey => {
+			//make an array of length same as qty of ingredient(igKey)
+			// eg: Bacon : 2, cheese : 1, salad : 1  (respectively undefined arrays [,], [], [])
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+			//For each element in array of undefined elements([,])
+			//with length same as of no. of ingredients add the JSX to render
+			return <BurgerIngredient key={igKey + i} type={igKey} />
+		})
+	})
+	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type='bread-top' />
-			<BurgerIngredient type='cheese' />
-			<BurgerIngredient type='meat' />
+			{transformedIngredients}
 			<BurgerIngredient type='bread-bottom' />
 		</div>
 	)
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,14 +4,29 @@ import Auxilliary from '../../components/hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
-     render() {
-         return (
-             <Auxilliary>
-                 <Burger />
-                 <div>Build Controls</div>
-             </Auxilliary>
-         )
-     }
+	// CAN USE A Constructor too
+	// constructor(props) {
+	//     super(props);
+	//     this.state = {...}
+	// }
+
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2,
+		}
+	}
+
+	render() {
+		return (
+			<Auxilliary>
+				<Burger ingredients={this.state.ingredients} />
+				<div>Build Controls</div>
+			</Auxilliary>
+		)
+	}
 }
 
 export default BurgerBuilder;
