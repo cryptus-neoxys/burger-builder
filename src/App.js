d250b6c8cbb4f457a168d6beb6cae469d2880701
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-09-08 17:46:41 +0530

    Started Routing for Checkout menu -Video 223

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 // import { useState } from 'react';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Layout>
         <BurgerBuilder />
       </Layout>
+      <Checkout />
     </div>
   );
 }
