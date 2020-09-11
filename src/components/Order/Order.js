27937c5b064868d264346f9c7dd2362c2926a25e
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-09-12 01:32:03 +0530

    Added Custom Dynamic Form element for checkout -Video236

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import classes from './Order.module.css';
 
-const order = (props) => (
+const order = (props) => {
+    const ingredients = [];
+
+    for (let key in props.ingredients) {
+        ingredients.push({name: key, amt: props.ingredients[key]})
+    }
+
+    console.log(ingredients);
+
+    const ingredientList = ingredients.map(ingredient => {
+        return <span 
+                key={ingredient.name}
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '2px solid #ccc',
+                    padding: '5px 10px',
+                    boxShadow: '1px 3px 2px #aaa'
+                }}> {ingredient.name} : {ingredient.amt} 
+                </span>
+        }
+        );
+
+    console.log(ingredientList);
+        
+    return (
     <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
+        <p>Ingreients: {ingredientList}</p>
+        <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
-)
+    )
+}
 
 export default order;
